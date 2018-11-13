8ab8970c52eda48559b934a986ff7479865172e2
Huego <hugozaanen@hotmail.com>
2018-11-13 12:29:54 +0100

    polgons

--- testje/MainWindow.xaml.cs
+++ testje/MainWindow.xaml.cs
@@ -23,59 +23,29 @@ namespace testje
     /// </summary>
     public partial class MainWindow : Window
     {
-        LocationConverter locConverter = new LocationConverter();
 
         public MainWindow()
         {
             InitializeComponent();
             //Set focus on the map
             myMap.Focus();
-            // Displays the current latitude and longitude as the map animates.
-            myMap.ViewChangeOnFrame += new EventHandler<MapEventArgs>(viewMap_ViewChangeOnFrame);
-            // The default animation level: navigate between different map locations.
-            //viewMap.AnimationLevel = AnimationLevel.Full;
+            addNewPolygon();
         }
 
-        private void viewMap_ViewChangeOnFrame(object sender, MapEventArgs e)
+        void addNewPolygon()
         {
-            // Gets the map object that raised this event.
-            Map map = sender as Map;
-            // Determine if we have a valid map object.
-            if (map != null)
-            {
-                // Gets the center of the current map view for this particular frame.
-                Location mapCenter = map.Center;
+            MapPolygon polygon = new MapPolygon();
 
-                // Updates the latitude and longitude values, in real time,
-                // as the map animates to the new location.
-                txtLatitude.Text = string.Format(CultureInfo.InvariantCulture,
-                  "{0:F5}", mapCenter.Latitude);
-                txtLongitude.Text = string.Format(CultureInfo.InvariantCulture,
-                    "{0:F5}", mapCenter.Longitude);
-            }
-        }
+            polygon.Fill = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.Blue);
+            polygon.Stroke = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.Green);
+            polygon.StrokeThickness = 5;
+            polygon.Opacity = 0.7;
+            polygon.Locations = new LocationCollection() {
+            new Location(50,100),
+            new Location(-200,200),
+            new Location(150,300) };
 
-        private void ChangeMapView_Click(object sender, RoutedEventArgs e)
-        {
-            // Parse the information of the button's Tag property
-            string[] tagInfo = ((Button)sender).Tag.ToString().Split(' ');
-            Location center = (Location)locConverter.ConvertFrom(tagInfo[0]);
-            double zoom = System.Convert.ToDouble(tagInfo[1]);
-
-            // Set the map view
-            myMap.SetView(center, zoom);
-
-        }
-
-        private void AnimationLevel_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            ComboBoxItem cbi = (ComboBoxItem)(((ComboBox)sender).SelectedItem);
-            string v = cbi.Content as string;
-            if (!string.IsNullOrEmpty(v) && myMap != null)
-            {
-                AnimationLevel newLevel = (AnimationLevel)Enum.Parse(typeof(AnimationLevel), v, true);
-                myMap.AnimationLevel = newLevel;
-            }
+            myMap.Children.Add(polygon);
         }
     }
 }
